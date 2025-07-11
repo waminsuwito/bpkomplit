b9a1270616e0ee77f41f2546e1542d16c3fce536
wamin suwito <waminsuwito1@gmail.com>
2025-07-11 18:52:13 +0000

    error

--- src/app/karyawan/manajemen-peralatan/page.tsx
+++ src/app/karyawan/manajemen-peralatan/page.tsx
@@ -1,7 +1,8 @@
-// This file previously contained Arduino C++ code by mistake, which caused a build error.
-// It has been replaced with a valid, empty React component to fix the build process.
-// The correct Arduino code can be found in the conversation history.
 
-export default function ManajemenPeralatanPagePlaceholder() {
-  return null;
+'use client';
+
+import { EditableVehicleList } from '@/components/karyawan/editable-vehicle-list';
+
+export default function ManajemenPeralatanPage() {
+  return <EditableVehicleList />;
 }
