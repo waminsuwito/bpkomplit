823da1c9aa5891ef101c4ea6ad0552ea5013ff94
wamin suwito <waminsuwito1@gmail.com>
2025-07-18 10:05:07 +0000

    halaman login tidak mau buka

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,12 +28,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="dark" suppressHydrationWarning>
-      <head>
+       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css2?family=Orbitron:wght@400..900&display=swap" rel="stylesheet" />
-
       </head>
       <body className="font-body antialiased">
         <AuthProvider>
